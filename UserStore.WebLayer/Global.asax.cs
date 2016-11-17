3c4961fdf40b2535f13608b5dc94ec8ac1355f27
Egor Nukrev <egor.nukrev@gmail.com>
2016-11-17 12:44:24 +0300

    Add log4net. Initialize. Implement Logger class

--- UserStore.WebLayer/Global.asax.cs
+++ UserStore.WebLayer/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 using System.Web.Optimization;
+using UserStore.BusinessLayer.Util;
 using UserStore.WebLayer.Util;
 
 namespace UserStore.WebLayer
@@ -17,6 +18,7 @@ namespace UserStore.WebLayer
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            Logger.InitLogger();
         }
     }
 }
